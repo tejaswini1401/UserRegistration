e03b77a6213cc38ad2974d2ec4117c07c23dd5c2
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-08-27 15:50:06 +0530

    Added: Valid Emial id with some test cases

--- com/test/TestCase.java
+++ com/test/TestCase.java
@@ -25,9 +25,27 @@ class TestCase {
 	@Test
 	void testEmailId() {
 		UserRegistration obj = new UserRegistration();
-		boolean result = obj.validEmailId("teja.shelake@abc.co.in");
-		assertTrue(result);
+		boolean result1 = obj.validEmailId("teja.shelake@abc.co.in");
+		boolean result2 = obj.validEmailId("teja.shelake@abc.com");
+		boolean result3 = obj.validEmailId("shelake@abc.com");
+
+		assertTrue(result1);
+		assertTrue(result2);
+		assertTrue(result3);
 		System.out.println("Valid EmailId");
+	}
+	@Test
+    void testInvalidEmails() {
+		UserRegistration obj = new UserRegistration();
+		boolean result1 = obj.validEmailId("teja.shelake");
+		boolean result2 = obj.validEmailId("teja.shelake.com");
+		boolean result3 = obj.validEmailId("shelake@abc@com");
+		//boolean result4 = obj.validEmailId("shelake@abc..com");
+		
+		assertFalse(result1, "Email without '@' and domain should be invalid");
+		assertFalse(result2, "Email without '@' before domain should be invalid");
+		assertFalse(result3, "Email with multiple '@' should be invalid");
+		//assertFalse(result4, "Email with multiple dots in domain should be invalid");
 	}
 	@Test
 	void testMobileNumber() {
@@ -40,6 +58,7 @@ class TestCase {
 	void testPassword() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.validPassword("Tejaswini@123");
+		
 		assertTrue(result);
 		System.out.println("Valid Password");
 	}
