84283c53b3beef149ef5ff2a8e27cc39c2a42ded
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-08-27 16:01:57 +0530

    Added: Junit Testing For Happy or Sad test cases

--- com/test/TestCase.java
+++ com/test/TestCase.java
@@ -34,19 +34,7 @@ class TestCase {
 		assertTrue(result3);
 		System.out.println("Valid EmailId");
 	}
-	@Test
-    void testInvalidEmails() {
-		UserRegistration obj = new UserRegistration();
-		boolean result1 = obj.validEmailId("teja.shelake");
-		boolean result2 = obj.validEmailId("teja.shelake.com");
-		boolean result3 = obj.validEmailId("shelake@abc@com");
-		//boolean result4 = obj.validEmailId("shelake@abc..com");
-		
-		assertFalse(result1, "Email without '@' and domain should be invalid");
-		assertFalse(result2, "Email without '@' before domain should be invalid");
-		assertFalse(result3, "Email with multiple '@' should be invalid");
-		//assertFalse(result4, "Email with multiple dots in domain should be invalid");
-	}
+	
 	@Test
 	void testMobileNumber() {
 		UserRegistration obj = new UserRegistration();
@@ -62,4 +50,38 @@ class TestCase {
 		assertTrue(result);
 		System.out.println("Valid Password");
 	}
+	
+	@Test
+    void testInvalidEmails() {
+		UserRegistration obj = new UserRegistration();
+		boolean result1 = obj.validEmailId("teja.shelake");
+		boolean result2 = obj.validEmailId("teja.shelake.com");
+		boolean result3 = obj.validEmailId("shelake@abc@com");
+		//boolean result4 = obj.validEmailId("shelake@abc..com");
+		
+		assertFalse(result1, "Email without '@' and domain should be invalid");
+		assertFalse(result2, "Email without '@' before domain should be invalid");
+		assertFalse(result3, "Email with multiple '@' should be invalid");
+		//assertFalse(result4, "Email with multiple dots in domain should be invalid");
+	}
+	
+	 @Test
+	 void testInvalidMobileNumber() {
+		 UserRegistration obj = new UserRegistration();
+
+	     assertFalse(obj.validMobileNumber("9028737812"), "Mobile Number without country code should be invalid");
+	     assertFalse(obj.validMobileNumber("91 90287378"), "Mobile Number with incorrect length should be invalid");
+	     assertFalse(obj.validMobileNumber("91 90287378123"), "Mobile Number with too many digits should be invalid");
+	 }
+	 @Test
+	 void testInvalidPassword() {
+		 UserRegistration obj = new UserRegistration();
+
+	     assertFalse(obj.validPassword("short"), "Password shorter than 8 characters should be invalid");
+	     assertFalse(obj.validPassword("NoSpecialChar1"), "Password without special character should be invalid");
+	     assertFalse(obj.validPassword("Multiple@Special@Char1"), "Password with multiple special characters should be invalid");
+	     //assertFalse(obj.validPassword("NoUppercase1@"), "Password without uppercase letter should be invalid");
+	     //assertFalse(obj.validPassword("NoLowercase1@"), "Password without lowercase letter should be invalid");
+	    // assertFalse(obj.validPassword("NoDigit@"), "Password without digit should be invalid");
+	 }
 }
